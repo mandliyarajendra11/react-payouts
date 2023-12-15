bf5d4d3f6c058acb57df437cdf15d29fff12a0f5
rajendra mandliya <mandliyarajendra0000@gmail.com>
2023-12-15 20:02:16 +0530

    build issue solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
-import svgrPlugin from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig(configEnv => ({
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
-    svgrPlugin(),
-  ],
+export default defineConfig(() => ({
+  plugins: [react(), tsConfigPaths(), svgrPlugin()],
+  build: {
+    rollupOptions: {
+      external: ["styled-components"],
+    },
+  },
   server: {
     port: 3000,
   },
   preview: {
     port: 8080,
   },
-}))
+}));
